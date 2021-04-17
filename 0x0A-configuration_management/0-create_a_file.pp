63878e47f814f2e4cad51208cf8ee149d9559825
mosesmrima <mrimamss@gmail.com>
2021-04-18 02:41:02 +0300

    fixed syntax err

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,11 +1,11 @@
 # Creates a file in /tmp
 
 
-file {'holberton'
-  ensure => 'present',
-  content => 'I love Puppet'
-  group => 'www-data',
-  mode => '0744'
-  owner => 'www-data'
-  path => '/tmp/holberton'
+file {'holberton':
+  ensure  => 'present',
+  content => 'I love Puppet',
+  group   => 'www-data',
+  mode    => '0744',
+  owner   => 'www-data',
+  path    => '/tmp/holberton',
 }

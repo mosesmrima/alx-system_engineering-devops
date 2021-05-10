21c0a232d831e65f01d84d9b842d266ad3081601
mosesmrima <mrimamss@gmail.com>
2021-05-10 17:03:35 +0300

    fixed PEP8 errors

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,30 +5,32 @@ It then serializes the json data to a python dictionary and prints it out
 It takes an parameter, the user Id of the user's data that you want
 """
 if __name__ == "__main__":
-    import urllib.request
     import json
     import sys
+    import urllib.request
 
     completed = 0
-    total_tasks = 0
-
-    with urllib.request.urlopen('https://jsonplaceholder.typicode.com/todos?userId={}'.format(sys.argv[1])) as response:
+    tasks = 0
+    url = 'https://jsonplaceholder.typicode.com/todos?userId={}'\
+        .format(sys.argv[1])
+    with urllib.request.urlopen(url) as response:
         json_data = response.read()
         dict_obj = json.loads(json_data)
         for jsonob in dict_obj:
             if jsonob['completed']:
                 completed += 1
-                total_tasks += 1
+                tasks += 1
             else:
-                total_tasks += 1
-
-    with urllib.request.urlopen('https://jsonplaceholder.typicode.com/users?id={}'.format(sys.argv[1])) as user_details:
+                tasks += 1
+    url = 'https://jsonplaceholder.typicode.com/users?id={}'.format(sys.argv[1])
+    with urllib.request.urlopen(url) as user_details:
         user_data = user_details.read()
         user_dict = json.loads(user_data)
         for data in user_dict:
             name = data['name']
-
-    print("Employee {} is done with tasks({}/{}):".format(name, completed, total_tasks))
+    output = "Employee {} is done with tasks({}/{}):".format(name, completed,
+                                                             tasks)
+    print(output)
     for task in dict_obj:
         if task['completed']:
             print("\t{}".format(task['title']))

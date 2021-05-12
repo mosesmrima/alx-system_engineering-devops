aa35e836f81f48f8cbe2ad8b6a118c15a1d84fa6
mosesmrima <mrimamss@gmail.com>
2021-05-12 15:26:50 +0300

    fixed url format

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,34 +4,15 @@ This script gathers data from an API using urllib
 It then serializes the json data to a python dictionary and prints it out
 It takes an parameter, the user Id of the user's data that you want
 """
+import sys
+import requests
 if __name__ == "__main__":
-    import json
-    import sys
-    import urllib.request
-
-    completed = 0
-    tasks = 0
-    url = 'https://jsonplaceholder.typicode.com/todos?userId={}'\
-        .format(sys.argv[1])
-    with urllib.request.urlopen(url) as response:
-        json_data = response.read()
-        dict_obj = json.loads(json_data)
-        for jsonob in dict_obj:
-            if jsonob['completed']:
-                completed += 1
-                tasks += 1
-            else:
-                tasks += 1
-    url = 'https://jsonplaceholder.typicode.com/users?id={}'\
-        .format(sys.argv[1])
-    with urllib.request.urlopen(url) as user_details:
-        user_data = user_details.read()
-        user_dict = json.loads(user_data)
-        for data in user_dict:
-            name = data['name']
-    output = "Employee {} is done with tasks({}/{}):".format(name, completed,
-                                                             tasks)
-    print(output)
-    for task in dict_obj:
-        if task['completed']:
-            print("\t {}".format(task['title']))
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos/',
+                        params={'userId': sys.argv[1]}).json()
+    user = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                        .format(sys.argv[1])).json()
+    completed = [task.get('title')
+                 for task in todo if task.get('completed') is True]
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user.get('name'), len(completed), len(todo)))
+    [print("\t {}".format(title)) for title in completed]

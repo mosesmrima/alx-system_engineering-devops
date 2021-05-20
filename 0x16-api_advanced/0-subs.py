64ee2e6895b7d5f9469737b5d0e3cde565191e34
mosesmrima <mrimamss@gmail.com>
2021-05-20 18:12:40 +0300

    reverted from querry string

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,10 +6,14 @@ import sys
 
 def number_of_subscribers(subreddit):
     headers = {"User-Agent": "aUserAgent"}
-    url = "https://www.reddit.com/subreddits/search.json?q={}"\
+    url = "https://www.reddit.com/r/{}.json"\
         .format(subreddit)
-    response = requests.get(url, headers=headers, allow_redirects=False).json()
-    try:
-        return response['data']['children'][0]['data']['subscribers']
-    except IndexError:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    status_code = response.status_code
+
+    if status_code == 200:
+        response_json = response.json()
+        count = response_json['data']['children'][0]['data']['subreddit_subscribers']
+        return count
+    else:
         return 0

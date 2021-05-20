e22861dd373270483875669d2b170d92cd252779
mosesmrima <mrimamss@gmail.com>
2021-05-20 13:45:27 +0300

    catch invalid subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 """This script gets number of subscribers from a subreddit"""
 import requests
-from sys import argv
+import sys
 
-headers = {"User-Agent": "aUserAgent"}
-url = "https://www.reddit.com/subreddits/search.json?q={}"\
-    .format(argv[1])
-subreddit = requests.get(url, headers=headers).json()
-try:
-    print(subreddit['data']['children'][0]['data']['subscribers'])
-except IndexError:
-    print(0)
+
+def number_of_subscribers(subreddit):
+    headers = {"User-Agent": "aUserAgent"}
+    url = "https://www.reddit.com/subreddits/search.json?q={}"\
+        .format(subreddit)
+    response = requests.get(url, headers=headers).json()
+    try:
+        return response['data']['children'][0]['data']['subscribers']
+    except IndexError:
+        return 0

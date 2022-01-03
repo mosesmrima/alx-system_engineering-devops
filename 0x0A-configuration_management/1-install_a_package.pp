65aef52712fd9681972dd5a6e6bf6fa8a62a65bf
mosesmrima <mrimamss@gmail.com>
2022-01-03 07:00:03 +0300

    reset tasks

--- 0x0A-configuration_management/1-install_a_package.pp
+++
@@ -1,6 +0,0 @@
-# Installs a package
-
-package { 'puppet-lint':
-  ensure => '2.1.1',
-  name   => 'puppet-lint',
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-# Execute a command
-
-
-exec {'pkill -f killmenow':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
-}
